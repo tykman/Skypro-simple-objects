91252298f712136c8d9619c898ca0037a4695e4a
tykman <nizom.ra@inbox.ru>
2023-05-15 22:36:27 +0500

    Homework.Simple-objects.Исправленное.

--- src/Book.java
+++ src/Book.java
@@ -1,15 +1,19 @@
 public class Book {
    private final String bookName;
+   private String authorName;
    private int publishingYear;
 
    public Book( String bookName, int publishingYear) {
       this.bookName = bookName;
+      this.authorName = authorName;
       this.publishingYear = publishingYear;
    }
 
    public String getBookName() {
       return this.bookName;
    }
+
+   public String getAuthorName() {return this.authorName;}
 
    public int getPublishingYear() {
       return this.publishingYear;
